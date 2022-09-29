64fc1089de0993ee86f6bf8c0969ad6f9626ab78
Martin Popper <martin.popper@siemens-healtineers.com>
2022-09-29 13:50:06 +0200

    Revert "Add coverage workflow"
    This reverts commit eb8f0ba5ad266a840281ca0f7281968e08012282.

--- angular-unit-tests-example/karma.conf.js
+++ angular-unit-tests-example/karma.conf.js
@@ -1,6 +1,5 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
   config.set({
@@ -30,8 +29,7 @@ module.exports = function (config) {
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' },
-        { type: 'lcov'}
+        { type: 'text-summary' }
       ]
     },
     reporters: ['progress', 'kjhtml'],
