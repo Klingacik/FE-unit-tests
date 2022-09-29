eb8f0ba5ad266a840281ca0f7281968e08012282
Martin Popper <martin.popper@siemens-healtineers.com>
2022-09-29 11:05:37 +0200

    Add coverage workflow

--- angular-unit-tests-example/karma.conf.js
+++ angular-unit-tests-example/karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
   config.set({
@@ -29,7 +30,8 @@ module.exports = function (config) {
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'text-summary' },
+        { type: 'lcov'}
       ]
     },
     reporters: ['progress', 'kjhtml'],
